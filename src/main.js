2c04ce9985e37c78c159c2921d008cd90cd289b6
qinyhquery <15927700473@163.com>
2018-01-27 10:47:23 +0800

    数据mock

---
+++ src/main.js
@@ -0,0 +1,28 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+Vue.config.productionTip = false
+
+
+// 导入重置样式
+import './common/stylus/icon.css';
+// 导入axios请求库
+import axios from 'axios';
+Vue.prototype.$axios = axios;
+
+// 导入axios的post请求参数序列化
+import qs from 'qs';
+Vue.prototype.$qs = qs;
+
+
+
+
+/* eslint-disable no-new */
+new Vue({
+    el: '#app',
+    router,
+    template: '<App/>',
+    components: { App }
+})
