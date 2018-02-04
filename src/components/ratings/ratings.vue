9b3df7bbca16799402948f0712841f354e0bd1c0
qinyhquery <15927700473@163.com>
2018-02-04 16:40:17 +0800

    完成seller.vue组件上部overview部分

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="ratings">
+	<div class="ratings" ref="ratings">
 		<div class="ratings-content">
 			<div class="overview">
 				<div class="overview-left">
@@ -26,6 +26,39 @@
 			</div>
 			<!-- 分割组件 -->
 			<split></split>
+			<!-- 评论选择组件 -->
+			<rating-select
+				:select-type="selectType"
+				:only-content="onlyContent"
+				:desc="desc"
+				:ratings="ratings"
+				@selectType="acceptType"
+				@onlyContent="acceptOnlyContent">
+			</rating-select>
+			<div class="rating-wrap">
+				<ul>
+					<li class="rating-item" v-for="(rating, index) in ratings" :key="index" v-show="needShow(rating.rateType, rating.text)">
+						<div class="avatar">
+							<img width="28" height="28" :src="rating.avatar">
+						</div>
+						<div class="content">
+							<h1 class="name">{{rating.username}}</h1>
+							<div class="star-wrap">
+								<v-star class="star" :size="24" :score="rating.score"></v-star>
+								<span class="delivery" v-show="rating.deliverTime">{{rating.deliverTime}}</span>
+							</div>
+							<p class="text">{{rating.text}}</p>
+							<div class="recommend" v-show="rating.recommend && rating.recommend.length">
+								<span class="icon-thumb_up"></span>
+								<span class="item" v-for="(item, index) in rating.recommend" :key="index">
+									{{item}}
+								</span>
+							</div>
+							<div class="time">{{rating.rateTime | datefmt('YYYY-MM-DD HH:mm:ss')}}</div>
+						</div>
+					</li>
+				</ul>
+			</div>
 		</div>
 	</div>
 </template>
@@ -35,6 +68,10 @@ import BScroll from 'better-scroll';
 import VStar from 'components/star/star';
 import split from 'components/split/split';
 import RatingSelect from 'components/ratingselect/rating-select';
+
+const POSITIVE = 0; // 好评
+const NEGATIVE = 1; // 吐槽
+const ALL = 2;
 export default {
 	components: {VStar, split, RatingSelect},
 	props: {
@@ -42,19 +79,64 @@ export default {
 	},
 	data() {
 		return {
-
+			ratings: [],
+			showFlag: false,
+        	selectType: ALL,
+        	onlyContent: true,
+        	desc: {
+        		all: '全部',
+        		positive: '满意',
+        		negative: '不满意'
+        	}
 		};
+	},
+	created() {
+		this.getRatings();
+	},
+	methods: {
+		getRatings() {
+			const url = '/api/ratings';
+			this.$axios.get(url).then(response => {
+				let data = response.data;
+				if (data.status === 1) {
+					this.ratings = data.data;
+					this.$nextTick(() => {
+						this.scroll = new BScroll(this.$refs.ratings, {
+							click: true
+						});
+					});
+				}
+			});
+		},
+		acceptType(data) {
+    		this.selectType = data;
+    	},
+    	acceptOnlyContent(data) {
+    		this.onlyContent = data;
+    	},
+		needShow(type, text) {
+			if (this.onlyContent && !text) {
+				return false;
+			}
+			if (this.selectType == ALL) {
+				return true;
+			} else {
+				return type === this.selectType;
+			}
+		}
 	}
 }
 </script>
 
 <style scoped lang="stylus">
+@import '../../common/stylus/mixin'
 	.ratings
 		position: absolute
 		top: 174px
 		bottom: 0
 		left: 0
 		right: 0
+		overflow: hidden
 		.overview
 			display: flex
 			padding: 18px 0
@@ -110,4 +192,62 @@ export default {
 						margin-left: 12px
 						font-size: 12px
 						color: rgb(147, 153, 159)
+		.rating-wrap
+			padding: 0 18px
+			.rating-item
+				display: flex
+				padding: 18px 0
+				border-1px(rgba(7, 17, 27, .2))
+				.avatar
+					flex: 0 0 28px
+					width: 28px
+					margin-right: 12px
+					img
+						border-radius: 50%
+				.content
+					position: relative
+					flex: 1
+					.name
+						margin-bottom: 4px
+						line-height: 12px
+						font-size: 10px
+						color: rgb(7, 17, 27)
+					.star-wrap
+						margin-bottom: 6px
+						font-size: 0
+						.star
+							display: inline-block
+							margin-right: 6px
+							vertical-align: middle
+						.delivery
+							display: inline-block
+							line-height: 12px
+							font-size: 10px
+							color: rgb(147, 153, 160)
+					.text
+						margin-bottom: 8px
+						line-height: 18px
+						color: rgb(7, 17, 27)
+						font-size: 12px
+					.recommend
+						line-height: 16px
+						font-size: 0
+						.icon-thumb_up, .item
+							display: inline-block
+							margin: 0 8px 4px 0
+							font-size: 9px
+						.icon-thumb_up
+							color: rgb(0, 160, 220)
+						.item
+							padding: 0 6px
+							border: 1px solid rgba(7, 17, 27, .1)
+							background: #fff
+							color: rgb(147, 153, 160)
+						.time
+							position: absolute
+							top: 0
+							right: 0
+							line-height: 12px
+							font-size: 10px
+							color: rgb(147, 153, 160)
 </style>
--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,17 +1,102 @@
 <template>
-    <div>
-		seller
-    </div>
+	<div class="seller" ref="seller">
+		<div class="seller-content">
+			<div class="overview">
+				<h1 class="title">{{seller.name}}</h1>
+				<div class="desc border-1px">
+					<v-star :size="36" :score="seller.score"></v-star>
+					<span class="text">（{{seller.ratingCount}}）</span>
+					<span class="text">月售{{seller.sellerCount}}单</span>
+				</div>
+				<ul class="remark">
+					<li class="block">
+						<h2>起送价</h2>
+						<div class="content">
+							<b class="stress">{{seller.minPrice}}元</b>
+						</div>
+					</li>
+					<li class="block">
+						<h2>商家配送</h2>
+						<div class="content">
+							<b class="stress">{{seller.deliveryPrice}}元</b>
+						</div>
+					</li>
+					<li class="block">
+						<h2>平均配送时间</h2>
+						<div class="content">
+							<b class="stress">{{seller.deliveryTime}}分钟</b>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </template>
-<script>
-export default {
-    data() {
-        return {
 
-        }
-    }
+<script>
+import VStar from 'components/star/star';
+export default {
+	components: {VStar},
+	props: {
+		seller: {type: Object}
+	},
+	data() {
+		return {
+
+		};
+	}
 }
 </script>
-<style scoped>
 
+<style scoped lang="stylus">
+@import '../../common/stylus/mixin'
+	.seller
+		position: absolute
+		top: 174px
+		bottom: 0
+		left: 0
+		right: 0
+		overflow: hidden
+		.overview
+			position: relative
+			padding: 18px
+			.title
+				margin-bottom: 8px
+				line-height: 14px
+				color: rgb(7, 17, 27)
+				font-size: 14px
+			.desc
+				padding-bottom: 18px
+				border-1px(rgba(7, 17, 27, .2))
+				font-size: 0
+				.star
+					display: inline-block
+					margin-right: 8px
+					vertical-align: top
+				.text
+					display: inline-block
+					margin-right: 12px
+					line-height: 18px
+					font-size: 10px
+					color: rgb(77, 85, 93)
+			.remark
+				display: flex
+				padding-top: 18px
+				.block
+					flex: 1
+					text-align: center
+					border-right: 1px solid rgba(7, 17, 27, .1)
+					&:last-of-type
+						border: none
+					h2
+						margin-bottom: 4px
+						line-height: 10px
+						font-size: 10px
+						color: rgb(147, 153, 159)
+					.content
+						line-height: 24px
+						font-size: 10px
+						color: rgb(7, 17, 27)
+						.stress
+							font-size: 24px
 </style>
